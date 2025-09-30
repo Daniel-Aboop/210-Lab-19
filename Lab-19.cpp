ad5f54a812cc804d9cc6e9e023a37e3d293692a7
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-09-30 16:28:24 -0700

    4:28 PM

--- Lab-19.cpp
+++ Lab-19.cpp
@@ -1,26 +1,41 @@
 // COMSC-210 | Lab 18 | Daniel Santisteban
 // IDE used: VS Code
 #include <iostream>
+#include <vector>
+#include <fstream>
 using namespace std;
 
 struct moviedata{
-
-
-
+    double rating;
+    string comment;
+    moviedata* next;
 };
-
-
+void output(moviedata *head);
+moviedata * choice2(moviedata *head,double rating,const string& comment);
 
 class Movie{
     private:
     string movietitle;
-
+    moviedata* head = nullptr;
 
 };
 
 int main(){
-
+    vector<Movie> list;
 
 
     return 0;
+}
+moviedata* choice2(moviedata *head,double rating,const string& comment){
+    moviedata* newNode = new moviedata{rating,comment,nullptr};
+    //if this is the first thing inside of our list it will become the new head.
+    if(head==nullptr){
+        return newNode;
+    }
+    moviedata* current = head;
+    while(current->next!=nullptr){
+        current=current->next;
+    }
+    current->next=newNode;
+    return head;
 }
