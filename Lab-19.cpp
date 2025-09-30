927f2a8e9d8d4b151138192e7a1f9ba54e2d7af8
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-09-30 16:18:18 -0700

    Start

---
+++ Lab-19.cpp
@@ -0,0 +1,26 @@
+// COMSC-210 | Lab 18 | Daniel Santisteban
+// IDE used: VS Code
+#include <iostream>
+using namespace std;
+
+struct moviedata{
+
+
+
+};
+
+
+
+class Movie{
+    private:
+    string movietitle;
+
+
+};
+
+int main(){
+
+
+
+    return 0;
+}
